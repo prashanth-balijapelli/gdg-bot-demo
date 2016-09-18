47a6c16c48aaa8c2018f61f2257b47d2260b00b9
gaurav sharma <prerna@prerna.local>
2016-09-18 20:08:20 +0530

    rest api to check server is running or not

--- app.js
+++ app.js
@@ -77,3 +77,7 @@ server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+server.get('/home',function(req,res){
+	res.send("server is running");
+});
