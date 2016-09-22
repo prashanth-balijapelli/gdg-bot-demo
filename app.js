257b467bb10dafeaa7662205deedc51b9491ef9a
gaurav sharma <prerna@prerna.local>
2016-09-22 16:57:02 +0530

    change port to 8080 for google cloud

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ bot.dialog('/profile', [
 var server = restify.createServer();
 
 server.post('/api/messages', connector.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || 8080, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
