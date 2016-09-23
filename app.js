654af03392ae01a959040b25d3918e854195deb6
gaurav sharma <prerna@prerna.local>
2016-09-23 05:36:19 +0530

    server issue

--- app.js
+++ app.js
@@ -147,10 +147,10 @@ bot.dialog('/', [
     function (session) {
         // Send a greeting and show help.
         var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
+            .title("GDG BOT")
+            .text("Your personal assistent for GDG Devfest HYD 2016.")
             .images([
-                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+                 builder.CardImage.create(session, "https://i.imgsafe.org/465769824b.jpg")
             ]);
         var msg = new builder.Message(session).attachments([card]);
         session.send(msg);
