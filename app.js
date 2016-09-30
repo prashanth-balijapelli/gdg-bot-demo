ede1a063809ef6d9a1354d0ada3bc2f476bae126
Prashanth B <prashanth@spotcues.com>
2016-09-30 10:29:36 +0530

    test commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
-// Create chat bot
+// Create chat bot :
 // var connector = new builder.ChatConnector({
 //     appId: process.env.MICROSOFT_APP_ID,
 //     appPassword: process.env.MICROSOFT_APP_PASSWORD
